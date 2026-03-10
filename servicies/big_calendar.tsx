5d4ccb69c020b6bcf9677f9376c8b3293611dc70
Stiximir <stiximir100@gmail.com>
2026-03-10 10:06:25 +0100

    Revert "done"
    This reverts commit b8132a671859f2ffb5f4d56cf67fa23362154af7.

--- servicies/big_calendar.tsx
+++ servicies/big_calendar.tsx
@@ -1,9 +1,9 @@
 import { supabase } from "@/lib/supabase";
 import { useFocusEffect } from "@react-navigation/native";
 import { useCallback, useState } from "react";
-import { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Calendar } from "react-native-big-calendar";
+import { GetRemoteEvents } from "./GetCalandar";
 
 export let event = [
   {
@@ -24,28 +24,10 @@ export async function registerEvent() {
     .contains("members", [user?.id]);
 
   if (!user || !groups?.[0]) return;
-    title: "Meeting",
-    start: new Date(2025, 11, 2, 10, 0),
-    end: new Date(2025, 11, 2, 10, 30),
-  },
-  {
-    title: "Test",
-    start: new Date(2026, 0, 28, 10, 0),
-    end: new Date(2026, 0, 28, 11, 30),
-  },
-];
-
-export async function createEvent() {
-  event.push({
-    title: "Nouveau Test " + new Date().getSeconds(),
-    start: new Date(2026, 0, 28, 9, 0),
-    end: new Date(2026, 0, 28, 10, 30),
-  });
-  console.log("Événement ajouté dans le tableau");
 }
 
 export function ShowCalendar() {
-  const [calendarEvent, setCalendarData] = useState(event);
+  const [calendarEvent, setCalendarData] = useState<any[]>([]);
 
   useFocusEffect(
     useCallback(() => {
@@ -54,11 +36,6 @@ export function ShowCalendar() {
       setCalendarData([...events]);
     }, []),
   );
-  function handleRefresh() {
-    createEvent();
-    setCalendarData([...event]);
-  }
-
   return (
     <View style={styles.container}>
       <Calendar
@@ -68,7 +45,6 @@ export function ShowCalendar() {
         swipeEnabled={true}
         onPressEvent={(e) => console.log(e)}
       />
-      {/* <Button title="Create Event" onPress={handleRefresh} /> */}
     </View>
   );
 }
